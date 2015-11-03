56222d1509f1ac33564b35902a3d26a56dbcc456
Daniel Neis Araujo <danielneis@gmail.com>
2015-11-03 13:45:11 -0200

    bump version

--- version.php
+++ version.php
@@ -32,4 +32,4 @@ $plugin->requires  = 2014051200;      // Requires this Moodle version.
 $plugin->cron      = 0;               // Period for cron to check this plugin (secs).
 $plugin->component = 'mod_webrtcexperiments'; // To check on upgrade, that plugin sits in correct place.
 $plugin->maturity  = MATURITY_RC;
-$plugin->release   = '0.5';
+$plugin->release   = '0.6';
